6dcc86f5ea9d3370160d585403ca6573848fdad1
kaunild <dhruv.kaunil@gmail.com>
2019-03-17 03:14:35 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      gen_count: 0
+    }
+
+    this.getWorksheet = this.getWorksheet.bind(this);
+  }
+
+  getWorksheet = () => {
+    console.log("fetching worksheet link");
+    var parent = this;
+    axios.post('http://localhost:8890/', { })
+    .then(function (response) {
+      parent.setState({gen_count: parent.state.gen_count+1});
+      window.open('http://localhost:8890/'+response.data.link);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button variant="outline-success" onClick={this.getWorksheet}>
+            {this.state.gen_count  === 0 ? 'Get Worksheet' : 'Another one!'}
+          </Button>
+
+
         </header>
       </div>
     );
